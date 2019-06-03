c1e3362fd063e48bae839b12c471367dfca4882d
Natali Donova <natali.donova@auriga.com>
2019-06-03 16:49:11 +0300

    4

---
+++ src/items/items.middleware.ts
@@ -0,0 +1,10 @@
+import { Injectable, NestMiddleware, MiddlewareConsumer } from '@nestjs/common';
+import { Request, Response } from 'express';
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: Function) {
+    console.log('Request...');
+    next();
+  }
+
+}
--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,6 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, RequestMethod } from '@nestjs/common';
+import { MiddlewareConsumer } from '@nestjs/common';
+
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
+import { LoggerMiddleware } from './items.middleware';
 
 @Module({
   imports: [],
@@ -9,4 +12,10 @@ import { ItemsService } from './items.service';
   ],
   providers: [ItemsService],
 })
-export class ItemsModule {}
+export class ItemsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'cats', method: RequestMethod.GET }); // or .forRoutes('cats');
+  }
+}
