64b84d2c9b73688b4e638728c8e14a749cd4c0bd
Natali Donova <natali.donova@auriga.com>
2019-07-16 17:33:00 +0300

    +

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const port = process.env.PORT || 3002;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log('Run on port:' + port);
 }
