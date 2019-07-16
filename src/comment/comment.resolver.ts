c27078af61c369303f258aca1141d746529e6403
Natali Donova <natali.donova@auriga.com>
2019-07-16 15:11:09 +0300

    +

--- src/comment/comment.resolver.ts
+++ src/comment/comment.resolver.ts
@@ -1,6 +1,5 @@
-import { Resolver, Query, Args, ResolveProperty, Parent, Mutation, Context } from '@nestjs/graphql';
+import { Resolver, Query, Args, Mutation, Context } from '@nestjs/graphql';
 import { UseGuards } from '@nestjs/common';
-import { UserService } from './user.service';
 import { CommentDTO } from '../comment/comment.dto';
 import { CommentService } from 'src/comment/comment.service';
 import { AuthGuard } from 'src/shared/auth.guard';
@@ -8,26 +7,35 @@ import { AuthGuard } from 'src/shared/auth.guard';
 @Resolver('comment')
 export class CommentResolver {
   constructor(
-    private userService: UserService,
     private commentService: CommentService,
   ) {
   }
 
   @Query()
-  comment(@Args('id') id: string) {
-    return this.commentService.show(id);
+  async comment(@Args('id') id: string) {
+    return await this.commentService.show(id);
   }
 
   @Mutation()
   @UseGuards(new AuthGuard())
-  createComment(
+  async createComment(
     @Args('idea') ideaId: string,
     @Args('comment') comment: string,
     @Context('user') user,
   ) {
     const data: CommentDTO = {comment};
     const {id: userId} = user;
-    return this.commentService.create(ideaId, userId, data);
+    return await this.commentService.create(ideaId, userId, data);
+  }
+
+  @Mutation()
+  @UseGuards(new AuthGuard())
+  async deleteComment(
+    @Args('id') id: string,
+    @Context('user') user,
+  ) {
+    const {id: userId} = user;
+    return await this.commentService.destroy(id, userId);
   }
 
 }
